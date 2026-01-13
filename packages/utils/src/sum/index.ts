4b36aacf7dd9de9b765c88ab3e31c893dbb3567c
Mitchell Wills <Mitchell.Wills@brightsparklabs.com>
2026-01-13 12:59:16 +1100

    BNL-4: Configured vite, vitest and package.json for the utils package. Added example "sum" util.

---
+++ packages/utils/src/sum/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Sums two numbers together.
+ *
+ * @param a The first number.
+ * @param b The second number.
+ * @returns The sum of the numbers.
+ */
+export function sum(a: number, b: number): number {
+  return a + b;
+}
