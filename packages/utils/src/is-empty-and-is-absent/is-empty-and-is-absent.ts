5526a547eebe1e2dc1a0f3827a81454e4e94c1d3
Cody Cronin-Sporys <cody.croninsporys@brightsparklabs.com>
2026-01-21 11:33:00 +1100

    BNL-12: Implement isAbsent function

---
+++ packages/utils/src/is-empty-and-is-absent/is-empty-and-is-absent.ts
@@ -0,0 +1,18 @@
+/*
+ * Maintained by brightSPARK Labs.
+ * www.brightsparklabs.com
+ */
+
+// ---------------------------------------------------------------------------
+// PUBLIC METHODS
+// ---------------------------------------------------------------------------
+  
+/**
+ * Returns `true` if the value is `null` or `undefined`.
+ *
+ * @param value The value to check.
+ * @returns `true` if the value is `null` or `undefined`, `false` otherwise.
+ */
+export function isAbsent<T>(value: T | null | undefined): value is undefined {
+  return value === null || value === undefined;
+}
