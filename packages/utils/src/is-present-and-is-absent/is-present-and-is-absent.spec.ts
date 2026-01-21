c541834b67016bcc363a36bed0f35d833634553c
Cody Cronin-Sporys <cody.croninsporys@brightsparklabs.com>
2026-01-21 12:41:15 +1100

    BNL-12: Add tests for isAbsentOrEmpty() function, organised all tests

--- packages/utils/src/is-present-and-is-absent/is-present-and-is-absent.spec.ts
+++ packages/utils/src/is-present-and-is-absent/is-present-and-is-absent.spec.ts
@@ -3,19 +3,47 @@
  * www.brightsparklabs.com
  */
 
-import { expect, test } from "vitest";
-import { isAbsent } from "./is-present-and-is-absent";
+import { describe, expect, test } from "vitest";
+import { isAbsent, isAbsentOrEmpty } from "./is-present-and-is-absent";
 
-test("isAbsent with undefined value, should be true", () => {
-    expect(isAbsent(undefined)).toBe(true);
+//  Testing isAbsent() with the 3 variations of input values
+// (absent, empty, present).
+describe("isAbsent()", () => {
+    test("Undefined value, should be true", () => {
+        expect(isAbsent(undefined)).toBe(true);
+    });
+
+    test ("Empty (\"\") value, should be false", () => {
+        expect(isAbsent("")).toBe(false);
+    });
+
+    test ("Present, non-empty (\"foo\") value, should be false", () => {
+        expect(isAbsent("foo")).toBe(false);
+    });
 });
 
-test ("isAbsent with empty (\"\") value, should be false", () => {
-    expect(isAbsent("")).toBe(false);
-});
+// Test isAbsentOrEmpty() with 5 variations of input values
+// (absent, empty string, empty array, present, present with empty [""]).
+describe("isAbsentOrEmpty()", () => {
+    test("Undefined value, should be true", () => {
+        expect(isAbsentOrEmpty(undefined)).toBe(true);
+    });
 
-test ("isAbsent with present, non-empty (\"\") value, should be false", () => {
-    expect(isAbsent("foo")).toBe(false);
+    test("Empty string (\"\") value, should be true", () => {
+        expect(isAbsentOrEmpty("")).toBe(true);
+    });
+
+    test("Empty array (\"\") value, should be true", () => {
+        expect(isAbsentOrEmpty([])).toBe(true);
+    });
+
+    test("Present, non-empty (\"\") value, should be false", () => {
+        expect(isAbsentOrEmpty("foo")).toBe(false);
+    });
+
+    test("Array containing empty string ([\"\"]) value, should be false", () => {
+        expect(isAbsentOrEmpty([""])).toBe(false);
+    });
 });
 
 
