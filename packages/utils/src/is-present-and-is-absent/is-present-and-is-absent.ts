edc80d9467f0473aa80d66fbbd4c07ea696e26e8
Cody Cronin-Sporys <cody.croninsporys@brightsparklabs.com>
2026-01-22 11:04:46 +1100

    BNL-12: Remove extra period from comment.

--- packages/utils/src/is-present-and-is-absent/is-present-and-is-absent.ts
+++ packages/utils/src/is-present-and-is-absent/is-present-and-is-absent.ts
@@ -58,7 +58,7 @@ export function isPresentAndNonEmpty<T>(value: T | null | undefined | ""): value
  * Returns `true` if the value is present and does NOT have a value,
  * i.e. `""`, `[]`.
  *
- * NOTE: Other types will return `true` e.g. A `number`, class or object irregardless..
+ * NOTE: Other types will return `true` e.g. A `number`, class or object irregardless.
  *
  * @param value The value to check.
  * @returns `true` if the value is `""` or `[]`, `false` otherwise.
