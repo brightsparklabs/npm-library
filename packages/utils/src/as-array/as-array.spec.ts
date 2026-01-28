2ef734871ef0a055b455a2b326bda19d00ed7ee1
Cody Cronin-Sporys <cody.croninsporys@brightsparklabs.com>
2026-01-28 14:04:06 +1100

    BNL-11: Add object type test cases.

--- packages/utils/src/as-array/as-array.spec.ts
+++ packages/utils/src/as-array/as-array.spec.ts
@@ -7,7 +7,7 @@ import { expect, test } from "vitest";
 import { asArray } from "./as-array";
 
 /*
- * asArray test cases.
+ * Test cases for asArray.
  */
 test("Undefined value, should return empty array ([])", () => {
   expect(asArray(undefined)).toStrictEqual([]);
@@ -28,3 +28,28 @@ test('Multiple value array, should return itself (["a", "b", "c"])', () => {
   const value:Array<string> = ["a", "b", "c"];
   expect(asArray(value)).toBe(value);
 });
+test('Single value object, should return enclosed in array ([{"key":"a"}])', () => {
+  const value: object = {"key": "a"};
+  expect(asArray(value)).toStrictEqual([value]);
+});
+test('Single empty value object, should return enclosed in array ([{}])', () => {
+  const value: object = {};
+  expect(asArray(value)).toStrictEqual([value]);
+});
+test('Multiple value object, should return enclosed in array ([{"a":1, "b":2}])', () => {
+  const value:object = {"a": 1, "b":2}
+  expect(asArray(value)).toStrictEqual([value]);
+});
+
+//Test cases for records/ dict which cause errors as asArray does not accept the record type.
+//These tests can still be used and should pass
+/*
+test('Single value record/ dict, should return enclosed in array ([{"a":1}])', () => {
+  const value: Record<string, number> = {"a": 1};
+  expect(asArray(value)).toStrictEqual([value]);
+});
+test('Multiple value record/ dict, should return enclosed in array ([{"a":1, "b":2}])', () => {
+  const value: Record<string, number> = {"a": 1, "b":2}
+  expect(asArray(value)).toStrictEqual([value]);
+});
+*/
