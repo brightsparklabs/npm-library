cc3e36b9efbf4b72917a44ed07bff6736b6fe432
Edwin Chen-Desailly <254813881+edwin-chendesailly-bsl@users.noreply.github.com>
2026-02-05 09:57:59 +1030

    BNL-8: Add tests for string and object signals using advanceTimersByTimeAsync()

--- packages/angular-toolkit/projects/lib/src/utils/debounced-signal/debounced-signal.spec.ts
+++ packages/angular-toolkit/projects/lib/src/utils/debounced-signal/debounced-signal.spec.ts
@@ -3,25 +3,98 @@
  * www.brightsparklabs.com
  */
 
-import { throttleTime } from "rxjs";
-import { TestScheduler } from "rxjs/testing";
+import { signal, runInInjectionContext, EnvironmentInjector } from "@angular/core";
+import { TestBed } from "@angular/core/testing";
+import { debouncedSignal } from './debounced-signal';
 
 describe("debouncedSignal", () => {
-  const testScheduler = new TestScheduler((actual, expected) => {
-    expect(actual).deep.equal(expected);
+  beforeEach(() => {
+    vi.useFakeTimers();
   });
 
-  // This test runs synchronously.
-  it("generates the stream correctly", () => {
-    testScheduler.run((helpers) => {
-      const { cold, time, expectObservable, expectSubscriptions } = helpers;
-      const e1 = cold(" -a--b--c---|");
-      const e1subs = "  ^----------!";
-      const t = time("   ---|       "); // t = 3
-      const expected = "-a-----c---|";
+  afterEach(() => {
+    vi.useRealTimers();
+  });
 
-      expectObservable(e1.pipe(throttleTime(t))).toBe(expected);
-      expectSubscriptions(e1.subscriptions).toBe(e1subs);
-    });
+  it("Should hold initialValue and debounce updates", async () => {
+    const injector = TestBed.inject(EnvironmentInjector);
+    const source = signal('data');
+    const debounced = runInInjectionContext(injector, () => 
+      debouncedSignal(source, 'data', 100)
+    );
+
+    expect(debounced()).toBe('data');
+    source.set('dataa');
+    expect(debounced()).toBe('data');
+
+    await vi.advanceTimersByTimeAsync(50);
+    expect(debounced()).toBe('data');
+
+    await vi.advanceTimersByTimeAsync(50);
+    expect(debounced()).toBe('dataa');
+  });
+
+  it("Should handle dynamic debounce time via signal", async () => {
+    const injector = TestBed.inject(EnvironmentInjector);
+    const source = signal('data');
+    const timeDelay = signal(100);
+    const debounced = runInInjectionContext(injector, () => 
+      debouncedSignal(source, 'data', timeDelay)
+    );
+
+    source.set('dataa');
+    await vi.advanceTimersByTimeAsync(50);
+
+    expect(debounced()).toBe('data');
+
+    timeDelay.set(500);
+    await vi.advanceTimersByTimeAsync(100);
+
+    expect(debounced()).toBe('data'); 
+
+    await vi.advanceTimersByTimeAsync(400);
+    
+    expect(debounced()).toBe('dataa');
+  });
+
+  it("Should hold initialValue and debounce updates for object data", async () => {
+    const injector = TestBed.inject(EnvironmentInjector);
+    const source = signal([1,2,3,4]);
+    const debounced = runInInjectionContext(injector, () => 
+      debouncedSignal(source, [1,2,3,4], 100)
+    );
+
+    expect(debounced()).toStrictEqual([1,2,3,4]);
+    source.set([1,2,3,4,5]);
+    expect(debounced()).toStrictEqual([1,2,3,4]);
+
+    await vi.advanceTimersByTimeAsync(50);
+    expect(debounced()).toStrictEqual([1,2,3,4]);
+
+    await vi.advanceTimersByTimeAsync(50);
+    expect(debounced()).toStrictEqual([1,2,3,4,5]);
+  });
+
+  it("Should handle dynamic debounce time via signal with object data", async () => {
+    const injector = TestBed.inject(EnvironmentInjector);
+    const source = signal([1,2,3,4]);
+    const timeDelay = signal(100);
+    const debounced = runInInjectionContext(injector, () => 
+      debouncedSignal(source, source(), timeDelay)
+    );
+
+    source.set([1,2,3,4,5]);
+    await vi.advanceTimersByTimeAsync(50);
+
+    expect(debounced()).toStrictEqual([1,2,3,4]);
+
+    timeDelay.set(500);
+    await vi.advanceTimersByTimeAsync(100);
+
+    expect(debounced()).toStrictEqual([1,2,3,4]); 
+
+    await vi.advanceTimersByTimeAsync(400);
+    
+    expect(debounced()).toStrictEqual([1,2,3,4,5]);
   });
 });
