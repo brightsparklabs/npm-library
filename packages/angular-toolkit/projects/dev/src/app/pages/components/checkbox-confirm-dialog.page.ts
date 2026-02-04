580fab7ba78c8dbd2405fd58e9a827157b34bef0
Cody Cronin-Sporys <cody.croninsporys@brightsparklabs.com>
2026-02-04 12:35:09 +1100

    BNL-22: Update dev page, Create some simple tests.

--- packages/angular-toolkit/projects/dev/src/app/pages/components/checkbox-confirm-dialog.page.ts
+++ packages/angular-toolkit/projects/dev/src/app/pages/components/checkbox-confirm-dialog.page.ts
@@ -6,20 +6,27 @@ import { DialogService} from "primeng/dynamicdialog";
 @Component({
   imports: [],
   template: `
-    <p>Checkbox</p>
+    <h2>Checkbox Testing Page</h2>
+    <p>Checked and Confirmed values for a previous dialog box 
+      will only update when next clicking the button</p>
     <button (click)="onClick()">click</button>
+    <p>Checked: {{ checkedValue }}</p>
+    <p>Confirmed: {{ confirmedValue }}</p>
   `,
   providers: [{provide: DialogService}],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class CheckboxConfirmDialogPage {
 
-  // eslint-disable-next-line jsdoc/require-jsdoc
+  /** String for displaying/ testing onClose output values (checked). */
+  checkedValue = "";
+  /** String for displaying/ testing onClose output values (confirmed). */
+  confirmedValue = "";
+  /** References the DialogService from providers. */
   dialogService = inject(DialogService);
 
-  // eslint-disable-next-line jsdoc/require-jsdoc
+  /** Display dialog and handles onClose logic. */
   onClick() { 
-    // eslint-disable-next-line jsdoc/require-jsdoc
     const ref = this.dialogService.open(CheckboxConfirmDialogComponent, {
      header: "Set some setting...",
      modal: true,
@@ -34,13 +41,11 @@ export default class CheckboxConfirmDialogPage {
    });
    
    ref?.onClose.subscribe((output: CheckBoxConfirmDialogOutput) => {
-     if (output.checked) {
-       return;
-     }
-     return;
+    this.checkedValue = output.checked.toString();
+    this.confirmedValue = output.confirmed.toString();
    });
 
-    };
+  };
 
   
 
--- packages/angular-toolkit/projects/lib/src/checkbox-confirm-dialog/checkbox-confirm-dialog.spec.ts
+++ packages/angular-toolkit/projects/lib/src/checkbox-confirm-dialog/checkbox-confirm-dialog.spec.ts
@@ -1,9 +1,10 @@
 import { describe, expect, test, it, Mocked } from "vitest";
+import { page } from 'vitest/browser'
 import { CheckboxConfirmDialogComponent, 
     CheckBoxConfirmDialogOutput } from "./checkbox-confirm-dialog.component"
 import { DialogService, DynamicDialogRef } from "primeng/dynamicdialog";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { inject, inputBinding } from "@angular/core";
+import { inject, inputBinding, signal } from "@angular/core";
 import { Dialog } from "primeng/dialog";
 
 describe('Checkbox', async () => {
@@ -12,36 +13,58 @@ describe('Checkbox', async () => {
     let dynamicDialogRefSpy: Mocked<DynamicDialogRef>;
  
     beforeEach(async () => {
-
-    const spy: Mocked<DynamicDialogRef> = {close: vi.fn()};
+    
+    //Mock service for the dialogService/ DynamicDialogRef.
+    const spy: Mocked<DynamicDialogRef> = {close: vi.fn(), onClose: vi.fn()};
     
     await TestBed.configureTestingModule({
       imports: [CheckboxConfirmDialogComponent],
       providers: [{provide: DynamicDialogRef, useValue: spy}],
     }).compileComponents();
 
+    //Setting a value for required input checkboxLabel.
     fixture = TestBed.createComponent(CheckboxConfirmDialogComponent, {
         bindings: [
-            inputBinding('checkboxLabel', () => "test" ),
+          //static binding  
+          inputBinding('checkboxLabel', () => "test" ),
         ],
     });
     component = fixture.componentInstance;
 
     dynamicDialogRefSpy = TestBed.inject(DynamicDialogRef) as Mocked<DynamicDialogRef>
 
-    //dsComponent = dsFixture.componentInstance
-    //component = fixture.componentInstance;
     await fixture.whenStable();
   });
 
-  it("should create", () => {
+  it("Should create.", () => {
     expect(component).toBeTruthy();
   });
 
-  it("onclose", () => {
-    //click on close button
-    //fixture.componentRef.setInput("checkboxLabel", "value");
-    //click
-    expect(dynamicDialogRefSpy).toHaveBeenCalledTimes(0)
+  it("Mock DynamicDialogRef.close callable.", () => {
+    const getCloseCalls = vi.spyOn(dynamicDialogRefSpy, 'close');
+    dynamicDialogRefSpy.close();
+    expect(getCloseCalls).toHaveBeenCalledTimes(1)
+  });
+
+  it("Checkbox exists and clickable/ checkable.", async () => {
+    const cb = page.getByRole('checkbox');
+    expect(cb).not.toBeChecked();
+    await cb.click()
+    expect(cb).toBeChecked();
+  });
+
+  it("confirm clickable, close called.", async () => {
+    const getCloseCalls = vi.spyOn(dynamicDialogRefSpy, 'close');
+    expect(getCloseCalls).toHaveBeenCalledTimes(0)
+    //Open?
+    //Click button.
+    await page.getByRole('button', {name: /confirm/i}).click();
+    //Check for calls.
+    expect(getCloseCalls).toHaveBeenCalledTimes(1)
+    //Get the confirmed and checked values.
+    //dynamicDialogRefSpy?.onClose.subscribe((output: CheckBoxConfirmDialogOutput) => {
+    //  expect(output.checked).toBe(false);
+    //  expect(output.confirmed).toBe(true);
+    //});
   });
 });
