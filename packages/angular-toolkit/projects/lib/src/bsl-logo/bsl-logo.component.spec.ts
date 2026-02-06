dea286c34a20dada3d841bf029cc83c1434372ff
Nicholas Boylan <nicholas.boylan@brightsparklabs.com>
2026-02-06 16:34:48 +1100

    BNL-17: Visual regression testing set up and working.

--- packages/angular-toolkit/projects/lib/src/bsl-logo/bsl-logo.component.spec.ts
+++ packages/angular-toolkit/projects/lib/src/bsl-logo/bsl-logo.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { page } from 'vitest/browser'
 import { BslLogoComponent } from './bsl-logo.component';
 
 describe('BslLogoComponent', () => {
@@ -20,4 +20,8 @@ describe('BslLogoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  test('Ensure there is no change to the logo', async () => {
+    await expect(page.getByTestId('bsl-logo')).toMatchScreenshot('logo-screenshot')
+  })
 });
