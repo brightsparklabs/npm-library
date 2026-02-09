ae2d74fccc775afd75b142424020fddefb5af522
Edwin Chen-Desailly <254813881+edwin-chendesailly-bsl@users.noreply.github.com>
2026-02-09 13:08:21 +1030

    BNL-8: Add a demo page to seperate utils section of sidenav

---
+++ packages/angular-toolkit/projects/dev/src/app/pages/utils/debounced-signal.page.ts
@@ -0,0 +1,47 @@
+/*
+ * Created by brightSPARK Labs
+ * www.brightsparklabs.com
+ */
+
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { debouncedSignal } from "@brightsparklabs/angular-toolkit";
+import { InputTextModule } from "primeng/inputtext";
+import { SliderModule } from "primeng/slider";
+
+/** The dev page for the thingo. */
+@Component({
+  imports: [FormsModule, SliderModule, InputTextModule],
+  template: `
+    <div style="padding: 1rem;">
+      <h2>Debounced Signal Demo</h2>
+
+      <input type="text" pInputText placeholder="Type here.." [(ngModel)]="text" />
+
+      <div>
+        <div>Raw: {{ text() }}</div>
+        <div style="font-weight: bold;">Debounced: {{ delayedText() }}</div>
+      </div>
+
+      <div style="display: flex; flex-direction: column; width: 12rem; gap: 1rem;">
+        <span>Delay: {{ delay() }}ms</span>
+        <p-slider [step]="250" [min]="0" [max]="2000" [(ngModel)]="delay" />
+      </div>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class DebouncedSignalPage {
+  // -----------------------------------------------------------------------------------------------
+  // INSTANCE VARIABLES
+  // -----------------------------------------------------------------------------------------------
+
+  /** String signal to be debounced. */
+  readonly text = signal("");
+
+  /** The time in milliseconds to debounce by. */
+  readonly delay = signal(1000);
+
+  /** The value of the debounced string signal. */
+  readonly delayedText = debouncedSignal(this.text, "", this.delay);
+}
--- packages/angular-toolkit/projects/lib/src/public-api.ts
+++ packages/angular-toolkit/projects/lib/src/public-api.ts
@@ -1,4 +1,5 @@
 /* Public API Surface of lib. */
 
-export * from "./components/hello-world/hello-world.component";
 export * from "./components/checkbox-confirm-dialog/checkbox-confirm-dialog.component";
+export * from "./components/hello-world/hello-world.component";
+export * from "./utils/debounced-signal/debounced-signal";
