d6ae25e573d8454ee8215df3a63db85aee47d57f
Mitchell Wills <Mitchell.Wills@brightsparklabs.com>
2026-02-13 11:02:04 +1100

    BNL-10: Moved highlight-text component into components folder.

--- packages/angular-toolkit/projects/lib/src/public-api.ts
+++ packages/angular-toolkit/projects/lib/src/public-api.ts
@@ -3,5 +3,5 @@
 export * from "./components/bsl-logo/bsl-logo.component";
 export * from "./components/checkbox-confirm-dialog/checkbox-confirm-dialog.component";
 export * from "./components/hello-world/hello-world.component";
+export * from "./components/highlight-text/highlight-text.component";
 export * from "./utils/debounced-signal/debounced-signal";
-export * from "./highlight-text/highlight-text.component";
