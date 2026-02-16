1300e1606d9c53a08740b7a76ae01630cc5f1466
Nicholas Boylan <nicholas.boylan@brightsparklabs.com>
2026-02-16 12:38:42 +1100

    BNL-26: Started using correct testing procedure. I can't get dropto to work yet. Issue with the Target listbox not appearing until interacted with. Discovered repeatable steps to replicate PickList duplicate bug. Detailed in a test but not written yet.

--- packages/angular-toolkit/projects/lib/src/columns-selector-dialog/columns-selector-dialog.component.spec.ts
+++ packages/angular-toolkit/projects/lib/src/columns-selector-dialog/columns-selector-dialog.component.spec.ts
@@ -12,174 +12,10 @@ const DEFAULT_COLUMNS: GenericTableData[] = [
 const MOCK_SAVED_PREFERENCES: GenericTableData[] = [
   { field: "firstName", header: "First name"},
   { field: "lastNameName", header: "Last name"},
+  { field: "dateOfBirth", name: "Date of birth"},
 ]
 
 describe('ColumnsSelectorDialogComponent tests with no saved preferences', async () => {
-  let component: ColumnsSelectorDialogComponent;
-  let fixture: ComponentFixture<ColumnsSelectorDialogComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ ColumnsSelectorDialogComponent ],
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ColumnsSelectorDialogComponent);
-
-    /** Set the values of the component. */
-    fixture.componentRef.setInput('visible', true);
-    fixture.componentRef.setInput('visibleColumns', DEFAULT_COLUMNS);
-    fixture.componentRef.setInput('columns', DEFAULT_COLUMNS);
-    component = fixture.componentInstance;
-
-    await fixture.whenStable();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  it('visibleColumns should equal columns if there is no saved preferences', () => {
-    expect(component.visibleColumns()).toEqual(component.columns());
-  });
-
-  it('modal should close after cancel', async () => {
-    expect(component.visible()).toBeTruthy();
-    await expect(page.getByRole("dialog")).toBeInTheDocument();
-
-    await page.getByRole("button", {name: "cancel"}).click();
-    await expect(page.getByRole("dialog")).not.toBeInTheDocument();
-    // (component as any)['handleCancel']();
-    expect(component.visible()).toBeFalsy();
-
-  });
-
-  it('no changes made, save should not change visibleColumns and modal should close', () => {
-    expect(component.visible()).toBeTruthy();
-    let visibleColumnsBeforeSave = structuredClone(component.visibleColumns());
-    (component as any)['handleSave']();
-    expect(component.visibleColumns()).toEqual(visibleColumnsBeforeSave);
-    expect(component.visible()).toBeFalsy();
-  });
-
-  it('no changes made, reset makes _visibleColumns reset to columns', () => {
-    (component as any)['handleReset']();
-    expect(component.columns()).toEqual((component as any)['_visibleColumns']());
-  });
-
-  it('changes made, cancel, changes should not be applied', () => {
-
-    const visibleColumnsBeforeChange = structuredClone(component.visibleColumns());
-    const _visibleColumnsBeforeChange = structuredClone((component as any)['_visibleColumns']());
-    const hiddenColumnsBeforeChange = structuredClone((component as any)['hiddenColumns']());
-
-    /** Manually move element from _visibleColumns to hiddenColumns to simulate drag/drop. */
-    let a:GenericTableData = ((component as any)['_visibleColumns']()).splice(1,1);
-    ((component as any)['hiddenColumns']()).push(a);
-
-    /** visibleColumns shouldn't have changed, _visibleColumns and hiddenColumns should have. */
-    expect(component.visibleColumns()).toEqual(visibleColumnsBeforeChange);
-    expect((component as any)['hiddenColumns']()).not.toEqual(hiddenColumnsBeforeChange);
-    expect((component as any)['_visibleColumns']()).not.toEqual(_visibleColumnsBeforeChange);
-
-    (component as any)['handleCancel']();
-
-    /** visibleChanges shouldn't have changed after cancel. */
-    expect(component.visibleColumns()).toEqual(visibleColumnsBeforeChange);
-
-  });
-
-  it('changes made, save, changes should be applied', () => {
-
-    const visibleColumnsBeforeChange = structuredClone(component.visibleColumns());
-    const _visibleColumnsBeforeChange = structuredClone((component as any)['_visibleColumns']());
-    const hiddenColumnsBeforeChange = structuredClone((component as any)['hiddenColumns']());
-
-    /** Manually move element from _visibleColumns to hiddenColumns to simulate drag/drop. */
-    let a:GenericTableData = ((component as any)['_visibleColumns']()).splice(1,1);
-    ((component as any)['hiddenColumns']()).push(a);
-
-    /** visibleColumns shouldn't have changed, _visibleColumns and hiddenColumns should have. */
-    expect(component.visibleColumns()).toEqual(visibleColumnsBeforeChange);
-    expect((component as any)['hiddenColumns']()).not.toEqual(hiddenColumnsBeforeChange);
-    expect((component as any)['_visibleColumns']()).not.toEqual(_visibleColumnsBeforeChange);
-
-    (component as any)['handleSave']();
-
-    /** visibleColumns should change after save. */
-    expect(component.visibleColumns()).not.toEqual(visibleColumnsBeforeChange);
-  });
-
-  it('changes made but not saved, reset makes _visibleColumns the same as columns', () => {
-
-    const visibleColumnsBeforeChange = structuredClone(component.visibleColumns());
-    const _visibleColumnsBeforeChange = structuredClone((component as any)['_visibleColumns']());
-    const hiddenColumnsBeforeChange = structuredClone((component as any)['hiddenColumns']());
-
-    /** Manually move element from _visibleColumns to hiddenColumns to simulate drag/drop. */
-    let a:GenericTableData = ((component as any)['_visibleColumns']()).splice(1,1);
-    ((component as any)['hiddenColumns']()).push(a);
-
-    /** visibleColumns shouldn't have changed, _visibleColumns and hiddenColumns should have. */
-    expect(component.visibleColumns()).toEqual(visibleColumnsBeforeChange);
-    expect((component as any)['hiddenColumns']()).not.toEqual(hiddenColumnsBeforeChange);
-    expect((component as any)['_visibleColumns']()).not.toEqual(_visibleColumnsBeforeChange);
-
-    (component as any)['handleReset']();
-
-    /** _visibleChanges should now equal _visibleChangesBeforeChange,visibleColumns and columns. */
-    expect((component as any)['_visibleColumns']()).toEqual(_visibleColumnsBeforeChange);
-    expect((component as any)['_visibleColumns']()).toEqual(visibleColumnsBeforeChange);
-    expect((component as any)['_visibleColumns']()).toEqual(component.columns());
-
-  });
-
-  it('all columns exist across hiddenColumns and _visibleColumns', () => {
-
-    /** Get the total number of columns, and total between _visibleColumns and hiddenColumns. */
-    let totalColumns = (component.columns()).length;
-    let sumOfColumnsBeforeChange = amountOfCols(component);
-
-    expect(sumOfColumnsBeforeChange).toEqual(totalColumns);
-
-    /** Make a change. */
-    let a:GenericTableData = ((component as any)['_visibleColumns']()).splice(1,1);
-    ((component as any)['hiddenColumns']()).push(a);
-
-    /** The sum of the columns in _visibleColumns and hiddenColumns shouldn't have changed. */
-    let sumOfColumnsAfterChange = amountOfCols(component);
-    expect(sumOfColumnsAfterChange).toEqual(totalColumns);
-
-  });
-
-  it('ensure each column field exists and is in either hiddenColumns or _visibleColumns', () => {
-
-    // TODO: Is this check redundant because of typescript type checking?
-    // Check that each column has a `field` field.
-    DEFAULT_COLUMNS.forEach(element => {
-      expect(element.field).toBeTruthy();
-    });
-
-    /** Get all the field names in columns, and in each of _visibleColumns and hiddenColumns. */
-    const allColumnNames:Array<string> = DEFAULT_COLUMNS.map(columns => columns.field);
-    const _visibleColumnsFields:Array<string> = ((component as any)['_visibleColumns']()).map((columns: { field: any; }) => columns.field);
-    const hiddenColumnsFields:Array<string> = ((component as any)['hiddenColumns']()).map((columns: { field: any; }) => columns.field);
-
-    /** Check that each field name in columns exists in either _visibleColumns or hiddenColumns. */
-    allColumnNames.forEach(element => {
-      expect([..._visibleColumnsFields, ...hiddenColumnsFields]).toContain(element);
-    });
-  });
-
-  it('Some tests relating to config for different input types', () => {
-
-
-
-  });
-  
-});
-
-describe('ColumnsSelectorDialogComponent tests with saved preferences', async () => {
   let component: ColumnsSelectorDialogComponent;
   let fixture: ComponentFixture<ColumnsSelectorDialogComponent>;
 
@@ -200,147 +36,118 @@ describe('ColumnsSelectorDialogComponent tests with saved preferences', async ()
     await fixture.whenStable();
   });
 
+  //DONE
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('visibleColumns should just be default columns', () => {
-
-    expect(component.visibleColumns()).not.toEqual(component.columns());
+  //DONE
+  it('visibleColumns should equal columns if there is no saved preferences', () => {
+    expect(component.visibleColumns()).toEqual(component.columns());
   });
 
-  it('modal should close after cancel is hit', () => {
-    expect(component.visible()).toBeTruthy();
-    (component as any)['handleCancel']();
+  //DONE. USE THIS AS AN EXAMPLE
+  it('modal should close after cancel', async () => {
+
+    // Check the modal is open
+    await expect(page.getByRole("dialog")).toBeInTheDocument();
+    // Click the cancel button
+    await page.getByRole("button", {name: "Cancel"}).click();
+    // Check the modal is closed
+    await expect(page.getByRole("dialog")).not.toBeInTheDocument();
+    // Check the visible flag is correctly updated
     expect(component.visible()).toBeFalsy();
   });
 
-  it('no changes made, no change to visibleColumns when saved, and modal should close', () => {
-    expect(component.visible()).toBeTruthy();
+  //DONE.
+  it('no changes made, save should not change visibleColumns and modal should close', async () => {
+
+    // Make a copy of visibleColumns before save
     let visibleColumnsBeforeSave = structuredClone(component.visibleColumns());
-    (component as any)['handleSave']();
+
+    // Click the save button
+    await page.getByRole("button", {name: "Save"}).click();
+
+    // visibleColumns is the same before and after save
     expect(component.visibleColumns()).toEqual(visibleColumnsBeforeSave);
+
+    // check the modal disapears after save
+    await expect(page.getByRole("dialog")).not.toBeInTheDocument();
+
+    // check the visible flag is correctly updated after save
     expect(component.visible()).toBeFalsy();
   });
 
-  it('no changes made, reset makes _visibleColumns reset to columns and not visibleColumns', () => {
-    (component as any)['handleReset']();
-    expect(component.columns()).toEqual((component as any)['_visibleColumns']());
-    expect(component.visibleColumns()).not.toEqual((component as any)['_visibleColumns']());
-  });
+  it('changes made, reset makes _visibleColumns reset to columns', async () => {    
+    
+    // Drag 'First name' into the hidden columns. NOT WORKING. TARGET IS NOT SHOWING UP I THINK
+    await page.getByRole("option", {name: "First name"}).dropTo(page.getByRole("listbox", {name: "Target"}));
 
-  it('changes made, cancel, changes should not be applied', () => {
+    // Ensure drag and drop worked. 'First name' should be in Target
+    expect(page.getByRole("listbox", {name: "Target"}).getByRole("option")).toHaveTextContent("First Name");
+    expect(page.getByRole("listbox", {name: "Source"}).getByRole("option")).not.toHaveTextContent("First Name");
 
-    const visibleColumnsBeforeChange = structuredClone(component.visibleColumns());
-    const _visibleColumnsBeforeChange = structuredClone((component as any)['_visibleColumns']());
-    const hiddenColumnsBeforeChange = structuredClone((component as any)['hiddenColumns']());
+    // Click reset.
+    await page.getByRole("button", {name: "Reset to default"}).click();
 
-    /** Manually move element from _visibleColumns to hiddenColumns to simulate drag/drop. */
-    let a:GenericTableData = ((component as any)['_visibleColumns']()).splice(1,1);
-    ((component as any)['hiddenColumns']()).push(a);
-
-    /** visibleColumns shouldn't have changed, _visibleColumns and hiddenColumns should have. */ 
-    expect(component.visibleColumns()).toEqual(visibleColumnsBeforeChange);
-    expect((component as any)['hiddenColumns']()).not.toEqual(hiddenColumnsBeforeChange);
-    expect((component as any)['_visibleColumns']()).not.toEqual(_visibleColumnsBeforeChange);
-
-    (component as any)['handleCancel']();
-
-    /** visibleChanges shouldn't have changed. */
-    expect(component.visibleColumns()).toEqual(visibleColumnsBeforeChange);
+    // First name should be in Source and not target
+    expect(page.getByRole("listbox", {name: "Source"}).getByRole("option")).toHaveTextContent("First Name");
+    expect(page.getByRole("listbox", {name: "Target"}).getByRole("option")).not.toHaveTextContent("First Name");
 
   });
 
-  it('changes made, save, changes should be applied', () => {
+  it('changes made, cancel, changes should not be applied', async () => {
 
-    const visibleColumnsBeforeChange = structuredClone(component.visibleColumns());
-    const _visibleColumnsBeforeChange = structuredClone((component as any)['_visibleColumns']());
-    const hiddenColumnsBeforeChange = structuredClone((component as any)['hiddenColumns']());
+    const visibleColumnsLengthBefore = component.visibleColumns().length;
 
-    /** Manually move element from _visibleColumns to hiddenColumns to simulate drag/drop. */
-    let a:GenericTableData = ((component as any)['_visibleColumns']()).splice(1,1);
-    ((component as any)['hiddenColumns']()).push(a);
+    // DRAG/DROP
+    // TODO: Drag first name from source to target
 
-    /** visibleColumns shouldn't have changed, _visibleColumns and hiddenColumns should have. */
-    expect(component.visibleColumns()).toEqual(visibleColumnsBeforeChange);
-    expect((component as any)['hiddenColumns']()).not.toEqual(hiddenColumnsBeforeChange);
-    expect((component as any)['_visibleColumns']()).not.toEqual(_visibleColumnsBeforeChange);
+    await page.getByRole("button", {name: "Cancel"}).click();
 
-    (component as any)['handleSave']();
-
-    /** visibleColumns should have changed. */
-    expect(component.visibleColumns()).not.toEqual(visibleColumnsBeforeChange);
+    // Check somehow that visibleColumns hasn't changed. Maybe the length?
+    // TODO: Does visibleColumnsLengthBefore update?
+    expect(visibleColumnsLengthBefore).toEqual(component.visibleColumns().length);
   });
 
-  it('changes made but not saved, reset makes _visibleColumns the same as columns', () => {
+  it('changes made, save, changes should be applied', async () => {
 
-    const visibleColumnsBeforeChange = structuredClone(component.visibleColumns());
-    const _visibleColumnsBeforeChange = structuredClone((component as any)['_visibleColumns']());
-    const hiddenColumnsBeforeChange = structuredClone((component as any)['hiddenColumns']());
+    const visibleColumnsLengthBefore = component.visibleColumns().length; 
 
-    /** Manually move element from _visibleColumns to hiddenColumns to simulate drag/drop. */
-    let a:GenericTableData = ((component as any)['_visibleColumns']()).splice(1,1);
-    ((component as any)['hiddenColumns']()).push(a);
+    //TODO: DRAG/DROP
 
-    /** visibleColumns shouldn't have changed, _visibleColumns and hiddenColumns should have. */
-    expect(component.visibleColumns()).toEqual(visibleColumnsBeforeChange);
-    expect((component as any)['hiddenColumns']()).not.toEqual(hiddenColumnsBeforeChange);
-    expect((component as any)['_visibleColumns']()).not.toEqual(_visibleColumnsBeforeChange);
+    await page.getByRole("button", {name: "Save"}).click();
 
-    (component as any)['handleReset']();
-
-    /**_visibleColumns should not equal _visibleChangesBeforeChange or visibleColumns. */
-    expect((component as any)['_visibleColumns']()).not.toEqual(_visibleColumnsBeforeChange);
-    expect((component as any)['_visibleColumns']()).not.toEqual(component.visibleColumns());
-    expect((component as any)['_visibleColumns']()).toEqual(component.columns());
-
+    /** visibleColumns should change after save. */
+    expect(visibleColumnsLengthBefore).not.toEqual(component.visibleColumns().length);
   });
 
 
-  //TODO: Test is not working because of issue in hiddenColumns definition.
-  it('correct amount of columns exist across hiddenColumns and _visibleColumns', () => {
+  it('duplicate bug when moving elements', async () => {
 
-    /** Get the total number of columns, and total between _visibleColumns and hiddenColumns. */
-    let totalColumns = (component.columns()).length;
-    let sumOfColumnsBeforeChange = amountOfCols(component);
-    console.log(`combined col lenght = ${sumOfColumnsBeforeChange}`)
-    console.log(`hidden length = ${((component as any)['hiddenColumns']()).length} and hid = ${((component as any)['hiddenColumns']())}`);
+    // Select one element from source list
 
-    expect(sumOfColumnsBeforeChange).toEqual(totalColumns);
+    // Drop it in target list
 
-    /** Make a change. */
-    let a:GenericTableData = ((component as any)['_visibleColumns']()).splice(1,1);
-    ((component as any)['hiddenColumns']()).push(a);
+    // Then using ctrl (or turn off metaKeySelection in HTML), select two elements from source list
 
-    /** The sum of the columns in _visibleColumns and hiddenColumns shouldn't have changed. */
-    let sumOfColumnsAfterChange = amountOfCols(component);
-    expect(sumOfColumnsAfterChange).toEqual(totalColumns);
+    // Drop both elements in target list
 
+    // The first element in target list should now be duplicated
+
+
+    // ADDITIONAL CASE:
+
+    // Select one element from source list, drop in target list
+
+    // Select another element in source list (ensure you highlight it, click on it once to highlight, and then again to drag, or hold ctrl on the first click)
+
+    // Drag second element over. First element will be duplicated
+
+    
+    // Both of these cases are repeatable in both the dev environment and on the primeng component page.
   });
 
-  it('ensure each column field exists and is in either hiddenColumns or _visibleColumns', () => {
-
-    // TODO: Is this check redundant because of typescript type checking?
-    // Check that each column has a `field` field.
-    DEFAULT_COLUMNS.forEach(element => {
-      expect(element.field).toBeTruthy();
-    });
-
-    /** Get all the field names in columns, and in each of _visibleColumns and hiddenColumns. */
-    const allColumnNames:Array<string> = DEFAULT_COLUMNS.map(columns => columns.field);
-    const _visibleColumnsFields:Array<string> = ((component as any)['_visibleColumns']()).map((columns: { field: any; }) => columns.field);
-    const hiddenColumnsFields:Array<string> = ((component as any)['hiddenColumns']()).map((columns: { field: any; }) => columns.field);
-
-    /** Check that each field name in columns exists in either _visibleColumns or hiddenColumns. */
-    allColumnNames.forEach(element => {
-      expect([..._visibleColumnsFields, ...hiddenColumnsFields]).toContain(element);
-    });
-  });
-
-  // TODO
-  it('Some tests relating to config for different input types', () => {
-
-  });
   
 });
 
@@ -382,15 +189,4 @@ describe('ColumnsSelectorDialogComponent edge cases', async () => {
   });
 
 });
-
-
-
-function amountOfCols(component: ColumnsSelectorDialogComponent):number {
-  console.log(`_vis length = ${((component as any)['_visibleColumns']()).length} and _vis = ${((component as any)['_visibleColumns']())}`);
-  console.log(`hidden length = ${((component as any)['hiddenColumns']()).length} and hid = ${((component as any)['hiddenColumns']())}`);
-  let sumOfColumns:number = ((component as any)['_visibleColumns']()).length + 
-      ((component as any)['hiddenColumns']()).length
-  return sumOfColumns;
-}
-
 
